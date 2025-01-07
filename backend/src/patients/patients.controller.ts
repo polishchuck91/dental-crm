934dd6bc2c13a161bf48390d4bfa81f7182b97a9
t0p91 <polishchuck91@gmail.com>
2025-01-07 13:36:00 +0200

    add roles to endpoints

--- backend/src/patients/patients.controller.ts
+++ backend/src/patients/patients.controller.ts
@@ -1,33 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { UpdatePatientDto } from './dto/update-patient.dto';
+import { Roles } from 'src/auth/roles.decorator';
+import { Role } from 'src/enums/role.enum';
 
 @Controller('patients')
 export class PatientsController {
   constructor(private readonly patientsService: PatientsService) {}
 
   @Post()
+  @Roles(Role.Admin, Role.Receptionist, Role.Dentist)
   create(@Body() createPatientDto: CreatePatientDto) {
     return this.patientsService.create(createPatientDto);
   }
 
   @Get()
+  @Roles(Role.Admin, Role.Receptionist, Role.Dentist)
   findAll() {
     return this.patientsService.findAll();
   }
 
   @Get(':id')
+  @Roles(Role.Admin, Role.Receptionist, Role.Dentist, Role.Patient)
   findOne(@Param('id') id: string) {
     return this.patientsService.findOne(+id);
   }
 
   @Patch(':id')
+  @Roles(Role.Admin, Role.Receptionist, Role.Dentist)
   update(@Param('id') id: string, @Body() updatePatientDto: UpdatePatientDto) {
     return this.patientsService.update(+id, updatePatientDto);
   }
 
   @Delete(':id')
+  @Roles(Role.Admin, Role.Receptionist, Role.Dentist)
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.patientsService.remove(+id);
   }
